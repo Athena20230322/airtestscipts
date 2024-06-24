b2de7b919d59f06bc44ef3752c18660e5a70f92a
P10381190 <autoicash2023@gmail.com>
2024-06-24 15:03:47 +0800

    updateairtest0624

---
+++ scripts/0621-1.air/0621-1.py
@@ -0,0 +1,30 @@
+# -*- encoding=utf8 -*-
+__author__ = "P10381190"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+
+from poco.drivers.android.uiautomation import AndroidUiautomationPoco
+poco = AndroidUiautomationPoco(use_airtest_input=True, screenshot_each_action=False)
+touch(Template(r"tpl1718958195598.png", record_pos=(-0.339, 0.065), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958198823.png", record_pos=(0.002, 0.249), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958200750.png", record_pos=(-0.101, 0.155), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958203508.png", record_pos=(0.034, 0.238), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958206867.png", record_pos=(0.106, 0.122), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958211107.png", record_pos=(0.042, 0.232), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958213534.png", record_pos=(0.374, 0.117), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958219838.png", record_pos=(-0.371, -0.216), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958226777.png", record_pos=(-0.419, -0.957), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958229342.png", record_pos=(-0.122, -0.21), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958232362.png", record_pos=(-0.001, 0.33), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958234679.png", record_pos=(0.106, -0.21), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958242862.png", record_pos=(-0.419, -0.966), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958245314.png", record_pos=(0.388, -0.219), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958248276.png", record_pos=(-0.419, -0.966), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958250432.png", record_pos=(-0.366, -0.037), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958252546.png", record_pos=(-0.419, -0.944), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958255809.png", record_pos=(-0.128, 0.014), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958259658.png", record_pos=(-0.431, -0.947), resolution=(1080, 2400)))
+touch(Template(r"tpl1718958262910.png", record_pos=(-0.417, -0.974), resolution=(1080, 2400)))
